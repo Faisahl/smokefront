cd3ba5a5513cbb3cb1bab46aa36c1bb6dae46771
Faisal Bokhari <Faisbokh@gmail.com>
2024-02-14 22:50:24 -0600

    .

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,10 +33,13 @@ module.exports = {
           55: '#04e762'
         },
         custo: {
-          50: '#65C18C',
+          50: '#42C2FF',
           51: '#4D77FF',
           52: '#FBFD8A'
         }
+      },
+      backgroundImage: {
+        'darkhow': "url('./public/images/pattern_batik-5_1_10_0-0_15_1__ffffff_42c3ff_4d76ff.png')"
       },
     },
   },
